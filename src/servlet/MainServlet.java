8e135e24ac09aa3a8dfcf499d88b4bfaf7733c90
plusdojo2024-Miyashita <miyashita-ryusuke-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-25 10:51:33 +0900

    宮下　0625　朝　更新

--- src/servlet/MainServlet.java
+++ src/servlet/MainServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class MainServlet
@@ -29,6 +30,16 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		int count;
+		try {
+			count = (int)session.getAttribute("mainCount")+1;
+		}
+		catch(Exception e) {
+			count = 0;
+		}
+		session.setAttribute("mainCount",count);
+		request.setAttribute("mainCount",0);
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 		dispatcher.forward(request, response);
@@ -39,7 +50,7 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
 	}
 
 }
